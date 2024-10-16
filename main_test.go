f515051a11cf1e231136a893ffefabf2272bbae3
PavamanDK <34388317+PavamanDK@users.noreply.github.com>
2024-10-16 20:11:26 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestMain(t *testing.T) {
-    expected := calc(2,2,"+") 
+    expected := calc(1,2,"+") 
     if expected != 3.0 {
         t.Fatalf("Expected %.2f, but got %0.2f", 3.0, expected)
     }
